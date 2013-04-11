c5eaa7e95747bf7c1492fdbb8e43c3160926e6ef
Jeff Williams <jeffrey.l.williams@gmail.com>
2013-04-11 16:06:24 -0700

    allow postfix nullable/non-nullable modifiers (#20)

--- test/specs/nullable.js
+++ test/specs/nullable.js
@@ -13,8 +13,28 @@ module.exports = [
 		}
 	},
 	{
+		description: 'postfix number or null',
+		expression: 'number?',
+		newExpression: '?number',
+		parsed: {
+			type: Types.NameExpression,
+			name: 'number',
+			nullable: true
+		}
+	},
+	{
 		description: 'object, not null',
 		expression: '!Object',
+		parsed: {
+			type: Types.NameExpression,
+			name: 'Object',
+			nullable: false
+		}
+	},
+	{
+		description: 'postfix object, not null',
+		expression: 'Object!',
+		newExpression: '!Object',
 		parsed: {
 			type: Types.NameExpression,
 			name: 'Object',
