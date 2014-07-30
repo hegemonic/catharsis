aa78c81c3598eaf3827acf554b80b23ac9dd0938
Jeff Williams <jeffrey.l.williams@gmail.com>
2014-07-30 10:01:16 -0700

    add JSON Schema for parsed types, and use it for validation

--- test/parser.js
+++ test/parser.js
@@ -6,9 +6,11 @@ var fs = require('fs');
 var helper = require('./helper');
 var parse = require('../lib/parser').parse;
 var path = require('path');
+var schema = require('../lib/schema');
 var should = require('should');
+var tv4 = require('tv4');
 var util = require('util');
-
+var validate = tv4.validateMultiple;
 
 function parseIt(item, options) {
 	var parsed;
@@ -23,28 +25,45 @@ function parseIt(item, options) {
 	if (!_.isEqual(parsed, item.parsed)) {
 		throw new Error(util.format('parse tree should be "%j", NOT "%j"', item.parsed, parsed));
 	}
+
+	return parsed;
 }
 
 function checkTypes(filepath, options) {
 	var types = require(filepath);
 
 	var errors = [];
+	var parsedType;
+	var parsedTypes = [];
+	var validationErrors = [];
+	var validationResult;
 
 	types.forEach(function(type) {
 		try {
-			parseIt(type, options);
+			parsedType = parseIt(type, options);
+			validationResult = validate(parsedType, schema, { banUnknownProperties: true });
+			if (validationResult.errors && validationResult.errors.length) {
+				validationErrors.push({
+					expression: type.expression,
+					errors: validationResult.errors
+				});
+			}
 		} catch(e) {
 			errors.push(e.message);
 		}
 	});
 
 	errors.should.eql([]);
+	validationErrors.should.eql([]);
 }
 
 describe('parser', function() {
 	describe('parse()', function() {
 		var specs = './test/specs';
 		var jsdocSpecs = path.join(specs, 'jsdoc');
+
+		// register schema with the validator
+		tv4.addSchema(schema.id, schema);
 
 		function tester(specPath, basename) {
 			it('can parse types in the "' + basename + '" spec', function() {
